0c32507872a1ab35ad5f0c8147f0cf27920f680e
Tanatorn Pravalpadd <71372664+picxy3d@users.noreply.github.com>
2022-11-28 22:44:33 +0700

    genflow_mini_alpha

--- RoboreactorGenFlow/configwriter_systemd.py
+++ RoboreactorGenFlow/configwriter_systemd.py
@@ -7,7 +7,7 @@ import configparser
 
 user = os.listdir("/home/")[0]
 try:
-   os.mkdir("/usr/lib/systemd/system", mode=0o777)
+    os.mkdir("/usr/lib/systemd/system", mode=0o777)
 except:
     print("System directory was created")
 Generate_path = "/usr/lib/systemd/system/"
@@ -45,122 +45,127 @@ list_board_os = ref_os_data.get("OS_board")
 print(list_support_os)
 print(list_board_os)
 if ar_kernel in list_support_os:
-      if list_board_os.get(ar_kernel) == "Jetson architecture":
-              settings = ['Unit', 'Service', 'Install']
-              # Unit
-              # Getting the section added into the list topic
-              config.add_section(settings[0])
-              config.set(settings[0], 'Description', str(project_name))
-              config.set(settings[0], 'After', str(mode))
-              # Service
-              config.add_section(settings[1])
-              config.set(settings[1], 'Type', 'idle')
-              config.set(settings[1], 'WorkingDirectory', Working_path)
-              config.set(settings[1], 'User', str(user))
-              config.set(settings[1], 'ExecStart', str(
-                  Python_exc_path1+Execute_path))
-              config.set(settings[1], 'WantedBy', 'always')
-              # Install
-              config.add_section(settings[2])
-              config.set(settings[2], 'WantedBy', str(mode))
-              configfile = open(
-                  Generate_path+"RoboreactorGenFlow.service", 'w')
-              config.write(configfile)
-      if list_board_os.get(ar_kernel) == "Raspberrypi architecture":
-              settings = ['Unit', 'Service', 'Install']
-              # Unit
-              # Getting the section added into the list topic
-              config.add_section(settings[0])
-              config.set(settings[0], 'Description', str(project_name))
-              config.set(settings[0], 'After', str(mode))
-              # Service
-              config.add_section(settings[1])
-              config.set(settings[1], 'Type', 'idle')
-              config.set(settings[1], 'WorkingDirectory', Working_path)
-              config.set(settings[1], 'User', str(user))
-              config.set(settings[1], 'ExecStart', str(
-                  Python_exc_path+Execute_path))
-              config.set(settings[1], 'WantedBy', 'always')
-              # Install
-              config.add_section(settings[2])
-              config.set(settings[2], 'WantedBy', str(mode))
-              configfile = open(
-                  Generate_path+"RoboreactorGenFlow.service", 'w')
-              config.write(configfile)
-     if list_board_os.get(ar_kernel) == "Laptop_PC":
-              settings = ['Unit','Service','Install']
-              # Unit
-              config.add_section(settings[0]) # Getting the section added into the list topic 
-              config.set(settings[0],'Description',str(project_name)) 
-              config.set(settings[0],'After',str(mode))
-              # Service 
-              config.add_section(settings[1])
-              config.set(settings[1],'Type','idle')
-              config.set(settings[1],'WorkingDirectory',Working_path)
-              config.set(settings[1],'User',str(user))
-              config.set(settings[1],'ExecStart',str(Python_exc_path1+Execute_path))
-              config.set(settings[1],'WantedBy','always') 
-              # Install 
-              config.add_section(settings[2])
-              config.set(settings[2],'WantedBy',str(mode))
-              configfile = open(Generate_path+"RoboreactorGenFlow.service",'w')
-              config.write(configfile)
-     if list_board_os.get(ar_kernel) == "Laptop_PC":
-              settings = ['Unit','Service','Install']
-              # Unit
-              config.add_section(settings[0]) # Getting the section added into the list topic 
-              config.set(settings[0],'Description',str(project_name)) 
-              config.set(settings[0],'After',str(mode))
-              # Service 
-              config.add_section(settings[1])
-              config.set(settings[1],'Type','idle')
-              config.set(settings[1],'WorkingDirectory',Working_path)
-              config.set(settings[1],'User',str(user))
-              config.set(settings[1],'ExecStart',str(Python_exc_path1+Execute_path))
-              config.set(settings[1],'WantedBy','always') 
-              # Install 
-              config.add_section(settings[2])
-              config.set(settings[2],'WantedBy',str(mode))
-              configfile = open(Generate_path+"RoboreactorGenFlow.service",'w')
-              config.write(configfile)
+    if list_board_os.get(ar_kernel) == "Jetson architecture":
+        settings = ['Unit', 'Service', 'Install']
+        # Unit
+        # Getting the section added into the list topic
+        config.add_section(settings[0])
+        config.set(settings[0], 'Description', str(project_name))
+        config.set(settings[0], 'After', str(mode))
+        # Service
+        config.add_section(settings[1])
+        config.set(settings[1], 'Type', 'idle')
+        config.set(settings[1], 'WorkingDirectory', Working_path)
+        config.set(settings[1], 'User', str(user))
+        config.set(settings[1], 'ExecStart', str(
+            Python_exc_path1+Execute_path))
+        config.set(settings[1], 'WantedBy', 'always')
+        # Install
+        config.add_section(settings[2])
+        config.set(settings[2], 'WantedBy', str(mode))
+        configfile = open(
+            Generate_path+"RoboreactorGenFlow.service", 'w')
+        config.write(configfile)
+    if list_board_os.get(ar_kernel) == "Raspberrypi architecture":
+        settings = ['Unit', 'Service', 'Install']
+        # Unit
+        # Getting the section added into the list topic
+        config.add_section(settings[0])
+        config.set(settings[0], 'Description', str(project_name))
+        config.set(settings[0], 'After', str(mode))
+        # Service
+        config.add_section(settings[1])
+        config.set(settings[1], 'Type', 'idle')
+        config.set(settings[1], 'WorkingDirectory', Working_path)
+        config.set(settings[1], 'User', str(user))
+        config.set(settings[1], 'ExecStart', str(Python_exc_path+Execute_path))
+        config.set(settings[1], 'WantedBy', 'always')
+        # Install
+        config.add_section(settings[2])
+        config.set(settings[2], 'WantedBy', str(mode))
+        configfile = open(Generate_path+"RoboreactorGenFlow.service", 'w')
+        config.write(configfile)
+    if list_board_os.get(ar_kernel) == "Laptop_PC":
+        settings = ['Unit', 'Service', 'Install']
+        # Unit
+        # Getting the section added into the list topic
+        config.add_section(settings[0])
+        config.set(settings[0], 'Description', str(project_name))
+        config.set(settings[0], 'After', str(mode))
+        # Service
+        config.add_section(settings[1])
+        config.set(settings[1], 'Type', 'idle')
+        config.set(settings[1], 'WorkingDirectory', Working_path)
+        config.set(settings[1], 'User', str(user))
+        config.set(settings[1], 'ExecStart', str(
+            Python_exc_path1+Execute_path))
+        config.set(settings[1], 'WantedBy', 'always')
+        # Install
+        config.add_section(settings[2])
+        config.set(settings[2], 'WantedBy', str(mode))
+        configfile = open(Generate_path+"RoboreactorGenFlow.service", 'w')
+        config.write(configfile)
+    if list_board_os.get(ar_kernel) == "Laptop_PC":
+        settings = ['Unit', 'Service', 'Install']
+        # Unit
+        # Getting the section added into the list topic
+        config.add_section(settings[0])
+        config.set(settings[0], 'Description', str(project_name))
+        config.set(settings[0], 'After', str(mode))
+        # Service
+        config.add_section(settings[1])
+        config.set(settings[1], 'Type', 'idle')
+        config.set(settings[1], 'WorkingDirectory', Working_path)
+        config.set(settings[1], 'User', str(user))
+        config.set(settings[1], 'ExecStart', str(
+            Python_exc_path1+Execute_path))
+        config.set(settings[1], 'WantedBy', 'always')
+        # Install
+        config.add_section(settings[2])
+        config.set(settings[2], 'WantedBy', str(mode))
+        configfile = open(Generate_path+"RoboreactorGenFlow.service", 'w')
+        config.write(configfile)
 
-     if list_board_os.get(ar_kernel) == "Khadas_VIMs":
-              settings = ['Unit','Service','Install']
-              # Unit
-              config.add_section(settings[0]) # Getting the section added into the list topic 
-              config.set(settings[0],'Description',str(project_name)) 
-              config.set(settings[0],'After',str(mode))
-              # Service 
-              config.add_section(settings[1])
-              config.set(settings[1],'Type','idle')
-              config.set(settings[1],'WorkingDirectory',Working_path)
-              config.set(settings[1],'User',str(user))
-              config.set(settings[1],'ExecStart',str(Python_exc_path1+Execute_path))
-              config.set(settings[1],'WantedBy','always') 
-              # Install 
-              config.add_section(settings[2])
-              config.set(settings[2],'WantedBy',str(mode))
-              configfile = open(Generate_path+"RoboreactorGenFlow.service",'w')
-              config.write(configfile)  
+    if list_board_os.get(ar_kernel) == "Khadas_VIMs":
+        settings = ['Unit', 'Service', 'Install']
+        # Unit
+        # Getting the section added into the list topic
+        config.add_section(settings[0])
+        config.set(settings[0], 'Description', str(project_name))
+        config.set(settings[0], 'After', str(mode))
+        # Service
+        config.add_section(settings[1])
+        config.set(settings[1], 'Type', 'idle')
+        config.set(settings[1], 'WorkingDirectory', Working_path)
+        config.set(settings[1], 'User', str(user))
+        config.set(settings[1], 'ExecStart', str(
+            Python_exc_path1+Execute_path))
+        config.set(settings[1], 'WantedBy', 'always')
+        # Install
+        config.add_section(settings[2])
+        config.set(settings[2], 'WantedBy', str(mode))
+        configfile = open(Generate_path+"RoboreactorGenFlow.service", 'w')
+        config.write(configfile)
 
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-config1 = configparser.ConfigParser() 
+config1 = configparser.ConfigParser()
 config1.optionxform = str
 # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Running remote_request_config
 # Unit
-config1.add_section(settings[0]) # Getting the section added into the list topic 
-config1.set(settings[0],'Description',"Request_remote_config") 
-config1.set(settings[0],'After',str(mode))
-# Service 
+# Getting the section added into the list topic
+config1.add_section(settings[0])
+config1.set(settings[0], 'Description', "Request_remote_config")
+config1.set(settings[0], 'After', str(mode))
+# Service
 config1.add_section(settings[1])
-config1.set(settings[1],'Type','idle')
-config1.set(settings[1],'WorkingDirectory',Working_path)
-config1.set(settings[1],'User',str(user))
-config1.set(settings[1],'ExecStart',str(Python_exc_path1+Execute_path1))
-config1.set(settings[1],'WantedBy','always')
-# Install 
+config1.set(settings[1], 'Type', 'idle')
+config1.set(settings[1], 'WorkingDirectory', Working_path)
+config1.set(settings[1], 'User', str(user))
+config1.set(settings[1], 'ExecStart', str(Python_exc_path1+Execute_path1))
+config1.set(settings[1], 'WantedBy', 'always')
+# Install
 config1.add_section(settings[2])
-config1.set(settings[2],'WantedBy',str(mode))
-configfile1 = open(Generate_path+"Remote_request_config.service",'w')
+config1.set(settings[2], 'WantedBy', str(mode))
+configfile1 = open(Generate_path+"Remote_request_config.service", 'w')
 config1.write(configfile1)
