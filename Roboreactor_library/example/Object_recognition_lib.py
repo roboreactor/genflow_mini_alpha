c3b1272fce2813ec89534e1594012e6be7f7709d
Chanapai Chuadchum <kornbot359@hotmail.com>
2022-12-09 19:55:25 +0700

    Update Object_recognition_lib.py

--- Roboreactor_library/example/Object_recognition_lib.py
+++ Roboreactor_library/example/Object_recognition_lib.py
@@ -5,7 +5,7 @@ from itertools import count
 model_prototxt = "MobileNetSSD_deploy.prototxt"
 model_caffe_weights = "MobileNetSSD_deploy.caffemodel"
 
-object_label = Object_labels = {0: 'background', 1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat', 5: 'bottle', 6: 'bus', 7: 'car', 8: 'cat', 9: 'chair', 10: 'cow',
+object_label = {0: 'background', 1: 'aeroplane', 2: 'bicycle', 3: 'bird', 4: 'boat', 5: 'bottle', 6: 'bus', 7: 'car', 8: 'cat', 9: 'chair', 10: 'cow',
                                 11: 'diningtable', 12: 'dog', 13: 'horse', 14: 'motorbike', 15: 'person', 16: 'pottedplant', 17: 'sheep', 18: 'sofa', 19: 'train', 20: 'tvmonitor'}
 
 
